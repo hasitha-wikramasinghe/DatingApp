15dc1cf10b6780518b20c0f883d0dc2714444cd0
Hasitha wikramasinghe <hasitha.wiky@gmail.com>
2022-05-16 00:54:48 +0530

    End of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -39,6 +39,9 @@ namespace API
             {
                 options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
             });
+
+            // Add CORS
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +57,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
